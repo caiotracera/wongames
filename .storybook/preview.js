2c61ee4c3fa0d5c138568d60a693a5a172ac75cb
Caio Tracera <ctrcra@gmail.com>
2022-04-19 10:09:47 -0300

    chore(storybook): add Storybook and it's configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+    </>
+  ),
+];
