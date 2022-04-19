ce2cbf398f916fbc24a836f7c020552f0ce623cc
Caio Tracera <ctrcra@gmail.com>
2022-04-19 10:56:41 -0300

    chore(storybook): update storybook to work with path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -11,8 +13,13 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  webpackFinal: (config) => {
-    config.resolve.modules.push(`${process.cwd()}/src`);
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
     return config;
   },
 };
