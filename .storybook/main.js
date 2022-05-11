76badc944dac1ecea0b2dc543169113e01285b32
Caio Tracera <ctrcra@gmail.com>
2022-05-11 14:39:15 -0300

    ci(renovate): remove unused dependency

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-next-router',
   ],
   framework: '@storybook/react',
   core: {
