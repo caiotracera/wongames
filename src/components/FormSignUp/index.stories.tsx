4796047beef86c549a0d3d0bd2a9223657c01af0
Caio Tracera <ctrcra@gmail.com>
2022-05-28 18:54:42 -0300

    feat(components): add FormSignUp

---
+++ src/components/FormSignUp/index.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { FormSignUp } from '@/components/FormSignUp';
+
+export default {
+  title: 'Form/FormSignUp',
+  component: FormSignUp,
+} as Meta;
+
+export const Default: Story = () => (
+  <div style={{ width: 300, margin: 'auto' }}>
+    <FormSignUp />
+  </div>
+);
---
+++ src/components/FormSignUp/index.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined';
+
+import { Button } from '@/components/Button';
+import { TextField } from '@/components/TextField';
+
+import * as S from '@/components/FormSignUp/styles';
+
+export function FormSignUp() {
+  return (
+    <S.Wrapper>
+      <form>
+        <TextField
+          name="name"
+          placeholder="Name"
+          type="text"
+          icon={<AccountCircle />}
+        />
+
+        <TextField
+          name="email"
+          placeholder="E-mail"
+          type="email"
+          icon={<Email />}
+        />
+
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          icon={<Lock />}
+        />
+
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+        />
+
+        <Button size="large" fullWidth>
+          Sign up now
+        </Button>
+
+        <S.FormLink>
+          Already have an account?
+          <Link href="/sign-in">
+            <a>Sign in</a>
+          </Link>
+        </S.FormLink>
+      </form>
+    </S.Wrapper>
+  );
+}
