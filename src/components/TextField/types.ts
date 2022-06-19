25f914431cac83761fd2d35947b2cc1e93af2226
Caio Tracera <ctrcra@gmail.com>
2022-06-19 05:39:44 -0300

    refactor(text field): replace labelName by name

--- src/components/TextField/types.ts
+++ src/components/TextField/types.ts
@@ -3,7 +3,6 @@ import { InputHTMLAttributes } from 'react';
 export type TextFieldProps = {
   onInput?: (value: string) => void;
   label?: string;
-  labelFor?: string;
   initialValue?: string;
   icon?: React.ReactNode;
   iconPosition?: 'left' | 'right';
